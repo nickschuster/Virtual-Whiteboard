3007252059ed90786bf5bc189bcb6096b27b2a21
Nick Schuster <ngpnickschster@gmail.com>
2020-09-27 14:56:14 -0700

    Created canvas class.

---
+++ js/app.js
@@ -0,0 +1,181 @@
+// Main javascript file for Virtual Whiteboard.
+import Canvas from './canvas';
+
+window.onload = () => {
+
+    let canvasList = [];
+    let activeCanvas;
+    let mouseX;
+    let mouseY;
+    let paint;
+
+    // Button listener for creating a new canvas.
+    function createCanvas() {
+        let root = document.getElementById("root");
+        let newCanvas = document.createElement("canvas");
+        newCanvas.setAttribute("id", "canvasOne");
+        root.appendChild(newCanvas);
+
+        let newCanvasObject = new Canvas("canvasOne");
+        canvasList.push(newCanvasObject);
+        activeCanvas = newCanvasObject;
+        activeCanvas.resize();
+    }
+
+    // Update mouse position in case of scroll.
+    $(window).on("scroll", (event) => {
+        if(lastScrolledLeft != $(document).scrollLeft()){
+            mouseX -= lastScrolledLeft;
+            lastScrolledLeft = $(document).scrollLeft();
+            mouseX += lastScrolledLeft;
+        }
+        if(lastScrolledTop != $(document).scrollTop()){
+            mouseY -= lastScrolledTop;
+            lastScrolledTop = $(document).scrollTop();
+            mouseY += lastScrolledTop;
+        }
+    });
+
+    // Keep track of the mouse position.
+    $(document).on("mousemove", (event) => {
+        mouseX = event.pageX;
+        mouseY = event.pageY;
+    });
+
+    // Listener for mousedown event. Start drawing.
+    $('canvas').on("mousedown", (event) => {
+        let paintX = mouseX - activeCanvas.context.canvas.offsetLeft;
+        let paintY = mouseY - activeCanvas.context.canvas.offsetTop;
+
+        paint = true;
+        activeCanvas.addClick(paintX, paintY, false);
+        activeCanvas.reDraw();
+    });
+
+    // Listener for mousemove event. If the mouse is being clicked
+    // start adding drag locations to be drawn.
+    $('canvas').on("mousemove", (event) => {
+        if(paint){
+            let paintX = mouseX - activeCanvas.context.canvas.offsetLeft;
+            let paintY = mouseY - activeCanvas.context.canvas.offsetTop;
+
+            activeCanvas.addClick(paintX, paintY, true);
+            activeCanvas.reDraw();
+        }
+    });
+
+    // Listener for mouseleave and mouseup. Stop drawing when mouse
+    // stops being on canvas or stops being clicked.
+    $('canvas').on("mouseleave mouseup", (event) => {
+        paint = false;
+    });
+
+
+
+    /// TEST ///
+    /*
+    // Reference to the canvas for drawing on.
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext('2d');
+
+    // Arrays that contain a list of mouse positions that need to be drawn.
+    let clickX = [];
+    let clickY = [];
+    let clickDrag = [];
+
+    // Contain the current mouse position relative to the window.
+    let mouseX;
+    let mouseY;
+
+    // Variable that tells drawing functions if it should be drawing right now.
+    // E.g. dont draw if the mouse is outside of the canvas.
+    let paint;
+
+    resize();
+
+    // Resize the canvas.
+    function resize() {
+        context.canvas.width = window.innerWidth*0.9;
+        context.canvas.height = window.innerHeight*0.9;
+
+        reDraw();
+    }
+
+    // Add mouse positions to drawing arrays.
+    function addClick(mouseX, mouseY, dragging) {
+        clickX.push(mouseX);
+        clickY.push(mouseY);
+        clickDrag.push(dragging);
+    }
+
+    // Draw all the positions to be drawn to the canvas.
+    function reDraw() {
+        context.strokeStyle = "#0076ff";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for(let i = 0; i < clickX.length; i++) {
+            context.beginPath();
+            if(clickDrag[i] && i) {
+                context.moveTo(clickX[i-1], clickY[i-1]);
+            } else {
+                context.moveTo(clickX[i], clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.stroke();
+            context.closePath();
+        }
+
+    }
+
+    // Resize canvas when window changes size.
+    //window.addEventListener("resize", resize);
+
+    // If the window is resized and a scroll ocours.
+    $(window).on("scroll", (event) => {
+        if(lastScrolledLeft != $(document).scrollLeft()){
+            mouseX -= lastScrolledLeft;
+            lastScrolledLeft = $(document).scrollLeft();
+            mouseX += lastScrolledLeft;
+        }
+        if(lastScrolledTop != $(document).scrollTop()){
+            mouseY -= lastScrolledTop;
+            lastScrolledTop = $(document).scrollTop();
+            mouseY += lastScrolledTop;
+        }
+    });
+
+    // Keep track of the mouse position.
+    $(document).on("mousemove", (event) => {
+        mouseX = event.pageX;
+        mouseY = event.pageY;
+    });
+
+    // Listener for mousedown event. Start drawing.
+    $('#canvas').on("mousedown", (event) => {
+        let paintX = mouseX - canvas.offsetLeft;
+        let paintY = mouseY - canvas.offsetTop;
+
+        paint = true;
+        addClick(paintX, paintY, false);
+        reDraw();
+    });
+
+    // Listener for mousemove event. If the mouse is being clicked
+    // start adding drag locations to be drawn.
+    $('#canvas').on("mousemove", (event) => {
+        if(paint){
+            let paintX = mouseX - canvas.offsetLeft;
+            let paintY = mouseY - canvas.offsetTop;
+
+            addClick(paintX, paintY, true);
+            reDraw();
+        }
+    });
+
+    // Listener for mouseleave and mouseup. Stop drawing when mouse
+    // stops being on canvas or stops being clicked.
+    $('#canvas').on("mouseleave mouseup", (event) => {
+        paint = false;
+    });*/
+};
--- js/canvas.js
+++ js/canvas.js
@@ -1,109 +1,46 @@
-// Main javascript file for Virtual Whiteboard.
+export default class Canvas {
 
-window.onload = () => {
+    constructor(canvasId) {
+        // Where to draw.
+        this.context = document.getElementById(canvasId).getContext('2d');
 
-    // Reference to the canvas for drawing on.
-    let canvas = document.getElementById("canvas");
-    let context = canvas.getContext('2d');
-
-    // Arrays that contain a list of mouse positions that need to be drawn.
-    let clickX = [];
-    let clickY = [];
-    let clickDrag = [];
-
-    // Contain the current mouse position relative to the window.
-    let mouseX;
-    let mouseY;
-
-    // Variable that tells drawing functions if it should be drawing right now.
-    // E.g. dont draw if the mouse is outside of the canvas.
-    let paint;
-
-    resize();
-
-    // Resize the canvas.
-    function resize() {
-        context.canvas.width = window.innerWidth*0.9;
-        context.canvas.height = window.innerHeight*0.9;
-
-        reDraw();
+        // Positions to draw.
+        this.clickX = [];
+        this.clickY = [];
+        this.clickDrag = [];
     }
 
-    // Add mouse positions to drawing arrays.
-    function addClick(mouseX, mouseY, dragging) {
+    // Add a click to the position arrays.
+    addClick(mouseX, mouseY, dragging) {
         clickX.push(mouseX);
         clickY.push(mouseY);
         clickDrag.push(dragging);
     }
 
-    // Draw all the positions to be drawn to the canvas.
-    function reDraw() {
-        context.strokeStyle = "#0076ff";
-        context.lineJoin = "round";
-        context.lineWidth = 5;
+    reDraw() {
+        this.context.strokeStyle = "#0076ff";
+        this.context.lineJoin = "round";
+        this.context.lineWidth = 5;
 
-        for(let i = 0; i < clickX.length; i++) {
-            context.beginPath();
-            if(clickDrag[i] && i) {
-                context.moveTo(clickX[i-1], clickY[i-1]);
+        for(let i = 0; i < this.clickX.length; i++) {
+            this.context.beginPath();
+            if(this.clickDrag[i] && i) {
+                this.context.moveTo(this.clickX[i-1], this.clickY[i-1]);
             } else {
-                context.moveTo(clickX[i], clickY[i]);
+                this.context.moveTo(this.clickX[i], this.clickY[i]);
             }
-            context.lineTo(clickX[i], clickY[i]);
-            context.stroke();
-            context.closePath();
+            this.context.lineTo(this.clickX[i], this.clickY[i]);
+            this.context.stroke();
+            this.context.closePath();
         }
 
     }
 
-    // Resize canvas when window changes size.
-    //window.addEventListener("resize", resize);
+    resize() {
+        this.context.canvas.width = window.innerWidth*0.9;
+        this.context.canvas.height = window.innerHeight*0.9;
 
-    // If the window is resized and a scroll ocours.
-    $(window).on("scroll", (event) => {
-        if(lastScrolledLeft != $(document).scrollLeft()){
-            mouseX -= lastScrolledLeft;
-            lastScrolledLeft = $(document).scrollLeft();
-            mouseX += lastScrolledLeft;
-        }
-        if(lastScrolledTop != $(document).scrollTop()){
-            mouseY -= lastScrolledTop;
-            lastScrolledTop = $(document).scrollTop();
-            mouseY += lastScrolledTop;
-        }
-    });
-
-    // Keep track of the mouse position.
-    $(document).on("mousemove", (event) => {
-        mouseX = event.pageX;
-        mouseY = event.pageY;
-    });
-
-    // Listener for mousedown event. Start drawing.
-    $('#canvas').on("mousedown", (event) => {
-        let paintX = mouseX - canvas.offsetLeft;
-        let paintY = mouseY - canvas.offsetTop;
-
-        paint = true;
-        addClick(paintX, paintY, false);
         reDraw();
-    });
+    }
 
-    // Listener for mousemove event. If the mouse is being clicked
-    // start adding drag locations to be drawn.
-    $('#canvas').on("mousemove", (event) => {
-        if(paint){
-            let paintX = mouseX - canvas.offsetLeft;
-            let paintY = mouseY - canvas.offsetTop;
-
-            addClick(paintX, paintY, true);
-            reDraw();
-        }
-    });
-
-    // Listener for mouseleave and mouseup. Stop drawing when mouse
-    // stops being on canvas or stops being clicked.
-    $('#canvas').on("mouseleave mouseup", (event) => {
-        paint = false;
-    });
-};
+}
