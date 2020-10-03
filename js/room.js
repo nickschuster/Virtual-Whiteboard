2d067dfde8c9ca517d52ac02d9874f96fa66a7ec
Nick Schuster <ngpnickschster@gmail.com>
2020-10-03 15:44:20 -0700

    Basic websocket communication.

--- js/room.js
+++ js/room.js
@@ -1,2 +1,14 @@
 // Create / Join a room.
 
+window.onload = () => {
+
+
+    $("#create-room").on('click', (event) => {
+
+        const socket = io("ws://localhost:3000")
+
+        socket.send("hello");
+        console.log("here");
+    })
+
+}
